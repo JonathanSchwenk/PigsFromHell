d87686d47b435ed8deff5ddc134a22d9d951cd33
Jonathan Schwenk <Jonathan@schwenk.net>
2023-08-17 09:22:49 -0400

    Redid survival menu ui and got screenshots.

---
+++ Assets/Scripts/Managers/SurvivalCanvasManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// mapSelected variable thats a string that gets set when a map button gets pressed. this variable tells which map should have the selected 
+// background vs the non-selected. This variable also will let the play button know which scene to load up.
+
+// SerializedField for a list of game objects that will be filled with the buttons so i can loop through this list and 
+// set the selected or not. 
+
+
+public class SurvivalCanvasManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
